07b61fad9f891528aaa3357d9f8c5e1850b2be17
UjjawalChaudhary <89735684+UjjawalChaudhary@users.noreply.github.com>
2023-03-21 12:04:23 +0530

    First

---
+++ ExpenseTrackerBackend-main/util/database.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize')
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'nodetutorial',
+//     password: 'apple@17'
+// });
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
+    dialect: 'mysql',
+    host: process.env.DB_HOST
+})
+
+module.exports = sequelize;
